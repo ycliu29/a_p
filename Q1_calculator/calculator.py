51767e1ea05724301a1b1dbdab2f6c97533b54c2
YCL <ycl@YCLs-mac-mini.hitronhub.home>
2022-04-03 21:44:58 +0800

    class updates

--- Q1_calculator/calculator.py
+++ Q1_calculator/calculator.py
@@ -1,14 +1,12 @@
-from promotion import Promotion
-
 class User:
-    def __init__(self, user_id):
+    def __init__(self, user_id: int):
         self.user_id = user_id
 
     def __str__(self):
         return 'User-{}'.format(self.user_id)
 
 class Product:
-    def __init__(self, product_id,product_unit_price):
+    def __init__(self, product_id:int,product_unit_price:int):
         self.product_id = product_id
         self.product_unit_price = product_unit_price
     
@@ -16,20 +14,19 @@ class Product:
         return 'Product-{}'.format(self.product_id)
 
 class Order:
-    def __init__(self, order_id, User, order_details, Promotion = None):
+    def __init__(self, order_id:int, order_details:dict[int:int]):
         self.order_id = order_id
-        self.order_user = User
-        # order_details 以 dictionary {'productID1':x, 'productID2':y} 形式儲存
+        #[productID1:quantity,productID2:quantity...etc]
         self.order_details = order_details
-        self.promotion = Promotion 
 
     # 計算訂單未折扣金額
-    def order_sum(self):
+    def order_sum(self) -> int:
         order_sum = 0
         for key,value in self.order_details.items():
             order_sum += key.product_unit_price*value
         return order_sum
 
+'''
 class Calculator:
     def __init__(self) -> None:
         pass
@@ -52,4 +49,4 @@ class Calculator:
                 deducted_sum = pre_promotion_sum - deduction
             return_d = {'pre_promotion_sum': pre_promotion_sum, 'deduction': deduction, 'deducted_sum': deducted_sum}
             return return_d
-    
+'''
