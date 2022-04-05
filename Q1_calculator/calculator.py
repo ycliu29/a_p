19359c15b315136d91c7d80e8f965ed9f505ef5f
YCL <ycl@YCLs-mac-mini.hitronhub.home>
2022-04-05 22:01:02 +0800

    logic update

--- Q1_calculator/calculator.py
+++ Q1_calculator/calculator.py
@@ -35,6 +35,13 @@ class Promotion:
         self.threshold = threshold
         self.discount = discount
 
+class Promotion_usage_limit(Promotion):
+    def __init__(self,promotion_id,threshold,discount,usage_limit,current_usage):
+        super().__init__(promotion_id, threshold, discount)
+        self.usage_limit = usage_limit
+        self.current_usage = current_usage
+
+# --- 
 # 抽象折扣資格檢查類別
 class Promo_Requirement_Checker(ABC):
     @abstractclassmethod
@@ -47,7 +54,7 @@ class Order_sum_Promo_Requirement_Checker(Promo_Requirement_Checker):
     def check(self, order:Order, promotion:Promotion) -> bool:
         return True if order.order_sum() >= promotion.threshold else False
 
-# 檢查 order 中是否有單一品項大於折扣所需門檻
+# 檢查 order 中是否有單一品項個數大於折扣門檻
 class Item_Amount_Promo_Requirement_Checker(Promo_Requirement_Checker):
     @classmethod
     def check(self, order:Order, promotion:Promotion) -> bool:
@@ -56,31 +63,58 @@ class Item_Amount_Promo_Requirement_Checker(Promo_Requirement_Checker):
                 return True
         return False
 
+# 檢查 order 總額是否大於門檻以及小於使用次數限制
+class Order_sum_usage_limit_Promo_Requirement_Checker(Promo_Requirement_Checker):
+    @classmethod
+    def check(self, order:Order, promotion:Promotion) -> bool:
+        if order.order_sum() >= promotion.threshold and promotion.current_usage < promotion.usage_limit:
+            return True
+        else:
+            return False
+
+# ---
 # 抽象折扣計算類別
 class Promo_Deduction_Calculator(ABC):
     @abstractclassmethod
     def calculate_deduction(self, order:Order, promotion:Promotion):
         pass
 
+# 折扣為折數 (x%)
 class Percentage_Promo_Deduction_Calculator(Promo_Deduction_Calculator):
     @classmethod
     def calculate_deduction(self, order:Order, promotion:Promotion)-> int:
         # 無條件捨去
-        deduction = int(order.order_sum() * promotion.discount)
-        return deduction
+        return int(order.order_sum() * promotion.discount)
 
+# 折扣為整數 ($50)
 class Sum_Promo_Deduction_Calculator(Promo_Deduction_Calculator):
     @classmethod
-    def calculate_deduction(self, order:Order, promotion:Promotion)-> int:
-        deduction = promotion.discount
-        return deduction
+    def calculate_deduction(self, promotion:Promotion)-> int:
+        return promotion.discount
 
+# 折扣為免費產品 (Product)
+class FreeItem_Promo_Deduction_Calculator(Promo_Deduction_Calculator):
+    @classmethod
+    def calculate_deduction(self, promotion:Promotion) -> Product:
+        return promotion.discount
+
+# ---
+# 抽象計算機類別
 class Calculator(ABC):
     @abstractclassmethod
     def calculate(cls,order:Order) -> dict:
         pass
 
-# 用於折抵總價類促銷
+# 無折扣促銷
+class Nopromo_Calculator(Calculator):
+    @classmethod
+    def calculate(cls, order:Order) -> dict:
+        pre_promotion_sum, deduction = order.order_sum(), 0
+        deducted_sum = pre_promotion_sum
+        return_dict = {'pre_promotion_sum': pre_promotion_sum, 'deduction': deduction, 'deducted_sum': deducted_sum}
+        return return_dict
+
+# 折抵總價類促銷（折扣總額不固定，如打折）
 class DiscountPromo_Calculator(Calculator):
     @classmethod
     def calculate(cls, order:Order, promotion:Promotion, promo_requirement:Promo_Requirement_Checker, promo_deduction:Promo_Deduction_Calculator) -> dict:
@@ -93,10 +127,30 @@ class DiscountPromo_Calculator(Calculator):
         return_dict = {'pre_promotion_sum': pre_promotion_sum, 'deduction': deduction, 'deducted_sum': deducted_sum}
         return return_dict
 
-class Nopromo_Calculator(Calculator):
+# 折抵總價類促銷（折扣總額固定）
+class FixedDiscountPromo_Calculator(Calculator):
     @classmethod
-    def calculate(cls, order:Order) -> dict:
-        pre_promotion_sum, deduction = order.order_sum(), 0
+    def calculate(cls, order:Order, promotion:Promotion, promo_requirement:Promo_Requirement_Checker, promo_deduction:Promo_Deduction_Calculator) -> dict:
+        pre_promotion_sum = order.order_sum()
+        if promo_requirement.check(order, promotion) == True:
+            deduction = promo_deduction.calculate_deduction(promotion)
+        else:
+            deduction = 0
+        deducted_sum = pre_promotion_sum - deduction
+        return_dict = {'pre_promotion_sum': pre_promotion_sum, 'deduction': deduction, 'deducted_sum': deducted_sum}
+        return return_dict
+
+# 贈品類促銷
+class FreeItem_Promo_Calculator(Calculator):
+    @classmethod
+    def calculate(cls, order:Order, promotion:Promotion,promo_requirement:Promo_Requirement_Checker, promo_deduction:Promo_Deduction_Calculator) -> dict:
+        pre_promotion_sum = order.order_sum()
+        if promo_requirement.check(order, promotion) == True:
+            deduction = promo_deduction.calculate_deduction(promotion)
+        else:
+            deduction = 0
         deducted_sum = pre_promotion_sum
         return_dict = {'pre_promotion_sum': pre_promotion_sum, 'deduction': deduction, 'deducted_sum': deducted_sum}
         return return_dict
+
+
--- Q1_calculator/test_calculator.py
+++ Q1_calculator/test_calculator.py
@@ -1,6 +1,5 @@
 import unittest
-from calculator import DiscountPromo_Calculator
-from calculator import User, Product, Order, Promotion, Promo_Requirement_Checker, Order_sum_Promo_Requirement_Checker, Item_Amount_Promo_Requirement_Checker,Promo_Deduction_Calculator, Percentage_Promo_Deduction_Calculator, Sum_Promo_Deduction_Calculator, Nopromo_Calculator
+from calculator import User, Product, Order, Promotion, Promotion_usage_limit, Promo_Requirement_Checker, Order_sum_Promo_Requirement_Checker, Item_Amount_Promo_Requirement_Checker, Order_sum_usage_limit_Promo_Requirement_Checker, Promo_Deduction_Calculator, Percentage_Promo_Deduction_Calculator, Sum_Promo_Deduction_Calculator, FreeItem_Promo_Deduction_Calculator, DiscountPromo_Calculator,  Nopromo_Calculator, FixedDiscountPromo_Calculator, FreeItem_Promo_Calculator
 
 # 單元測試
 class TestOrderClass(unittest.TestCase):
@@ -35,6 +34,17 @@ class TestItem_Amount_Promo_Requirement_Checker(unittest.TestCase):
         self.assertEqual(Item_Amount_Promo_Requirement_Checker.check(self.order1,self.promotion1),True)
         self.assertEqual(Item_Amount_Promo_Requirement_Checker.check(self.order1,self.promotion2),False)
 
+class TestOrder_sum_usage_limit_Promo_Requirement_Checker(unittest.TestCase):
+    def setUp(self):
+        self.product1 = Product(product_id=1,product_unit_price=100)
+        self.order1 = Order(order_id=1,order_details={self.product1:5})
+        self.promotion1 = Promotion_usage_limit(promotion_id=1,threshold=300,discount=50,usage_limit=10,current_usage=5)
+        self.promotion2 = Promotion_usage_limit(promotion_id=2,threshold=300,discount=50,usage_limit=5,current_usage=5)
+
+    def test_check(self):
+        self.assertEqual(Order_sum_usage_limit_Promo_Requirement_Checker.check(self.order1,self.promotion1),True)
+        self.assertEqual(Order_sum_usage_limit_Promo_Requirement_Checker.check(self.order1,self.promotion2),False)
+
 class TestPercentage_Promo_Deduction_Calculator(unittest.TestCase):
     def setUp(self):
         self.product1 = Product(product_id=1,product_unit_price=70)
@@ -46,14 +56,20 @@ class TestPercentage_Promo_Deduction_Calculator(unittest.TestCase):
         self.assertEqual(Percentage_Promo_Deduction_Calculator.calculate_deduction(self.order1,self.promotion1),56)
         self.assertEqual(Percentage_Promo_Deduction_Calculator.calculate_deduction(self.order1,self.promotion2),39)
 
-class TestSum_Promo_Deduction_Calculator(unittest.TestCase):
+class TestFreeItem_Promo_Deduction_Calculator(unittest.TestCase):
     def setUp(self):
         self.product1 = Product(product_id=1,product_unit_price=70)
-        self.order1 = Order(order_id=1,order_details={self.product1:8})
+        self.promotion1 = Promotion(promotion_id=1,threshold=500,discount=self.product1)
+    
+    def test_calculate_deduction(self):
+        self.assertEqual(FreeItem_Promo_Deduction_Calculator.calculate_deduction(self.promotion1),self.product1)
+
+class TestSum_Promo_Deduction_Calculator(unittest.TestCase):
+    def setUp(self):
         self.promotion1 = Promotion(promotion_id=1,threshold=500,discount=30)
     
     def test_calculate_deduction(self):
-        self.assertEqual(Sum_Promo_Deduction_Calculator.calculate_deduction(self.order1, self.promotion1),30)
+        self.assertEqual(Sum_Promo_Deduction_Calculator.calculate_deduction(self.promotion1),30)
 
 class TestNopromo_CalculatorClass(unittest.TestCase):
     def setUp(self):
@@ -65,20 +81,46 @@ class TestNopromo_CalculatorClass(unittest.TestCase):
         self.assertEqual(Nopromo_Calculator.calculate(self.order1),{'pre_promotion_sum': 250, 'deduction': 0, 'deducted_sum': 250})
 
 # 整合測試
+# 適用場景：訂單滿 X 元折 Z %
 class TestDiscountPromo_Calculator(unittest.TestCase):
     def setUp(self):
         self.product1 = Product(product_id=1,product_unit_price=10)
         self.product2 = Product(product_id=2, product_unit_price=20)
         self.order1 = Order(order_id=1,order_details={self.product1:5,self.product2:10})
         self.promotion1 = Promotion(promotion_id=1,threshold=200,discount=0.1)
-        self.promotion2 = Promotion(promotion_id=2,threshold={self.product1:5},discount=30)
     
     def test_calculate(self):
         # 訂單滿 X 元折 Z %
         self.assertEqual(DiscountPromo_Calculator.calculate(self.order1,self.promotion1,Order_sum_Promo_Requirement_Checker,Percentage_Promo_Deduction_Calculator),{'pre_promotion_sum': 250, 'deduction': 25, 'deducted_sum': 225})
 
+# 適用場景：特定商品滿 X 件折 Y 元、滿 X 元折 Y 元，在全站總共只能套⽤ N 次
+class TestFixedDiscountPromo_Calculator(unittest.TestCase):
+    def setUp(self):
+        self.product1 = Product(product_id=1,product_unit_price=10)
+        self.product2 = Product(product_id=2, product_unit_price=20)
+        self.order1 = Order(order_id=1,order_details={self.product1:5,self.product2:10})
+        self.promotion1 = Promotion(promotion_id=1,threshold={self.product1:5},discount=30)
+        self.promotion2 = Promotion_usage_limit(promotion_id=2,threshold=200,discount=20,usage_limit=5,current_usage=3)
+        self.promotion3 = Promotion_usage_limit(promotion_id=3,threshold=200,discount=20,usage_limit=5,current_usage=5)
+    
+    def test_calculate(self):
         # 特定商品滿 X 件折 Y 元
-        self.assertEqual(DiscountPromo_Calculator.calculate(self.order1,self.promotion2,Item_Amount_Promo_Requirement_Checker,Sum_Promo_Deduction_Calculator),{'pre_promotion_sum': 250, 'deduction': 30, 'deducted_sum': 220})
+        self.assertEqual(FixedDiscountPromo_Calculator.calculate(self.order1,self.promotion1,Item_Amount_Promo_Requirement_Checker,Sum_Promo_Deduction_Calculator),{'pre_promotion_sum': 250, 'deduction': 30, 'deducted_sum': 220})
+        # 滿 X 元折 Y 元，在全站總共只能套⽤ N 次
+        self.assertEqual(FixedDiscountPromo_Calculator.calculate(self.order1,self.promotion2,Order_sum_usage_limit_Promo_Requirement_Checker,Sum_Promo_Deduction_Calculator),{'pre_promotion_sum': 250, 'deduction': 20, 'deducted_sum': 230})
+        self.assertEqual(FixedDiscountPromo_Calculator.calculate(self.order1,self.promotion3,Order_sum_usage_limit_Promo_Requirement_Checker,Sum_Promo_Deduction_Calculator),{'pre_promotion_sum': 250, 'deduction': 0, 'deducted_sum': 250})
+
+# 適用場景：訂單滿 X 元贈送特定商品
+class TestFreeItem_Promo_Calculator(unittest.TestCase):
+    def setUp(self):
+        self.product1 = Product(product_id=1,product_unit_price=10)
+        self.product2 = Product(product_id=2, product_unit_price=20)
+        self.order1 = Order(order_id=1,order_details={self.product1:5,self.product2:10})
+        self.promotion1 = Promotion(promotion_id=1,threshold=200,discount=self.product1)
+
+    def test_calculate(self):
+        # 訂單滿 X 元贈送特定商品
+        self.assertEqual(FreeItem_Promo_Calculator.calculate(self.order1,self.promotion1,Order_sum_Promo_Requirement_Checker,FreeItem_Promo_Deduction_Calculator),{'pre_promotion_sum': 250, 'deduction': self.product1, 'deducted_sum': 250})
 
 if __name__ == "__main__":
     unittest.main()
